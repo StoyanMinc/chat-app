afecc355f66774ef16331b7aec3162ea6d967798
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-04-23 23:58:37 +0300

    server initialization and create connection to mongoDB

---
+++ server/src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+import { connectDB } from './lib/db.js';
+import router from './router.js';
+
+dotenv.config();
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(express.json());
+app.use(router)
+
+app.get('/', (req, res) => {
+    res.send('hi')
+});
+
+app.listen(PORT, () => {
+    console.log(`SERVER IS LISTENING ON PORT: ${PORT}...`)
+    connectDB();
+});
+
