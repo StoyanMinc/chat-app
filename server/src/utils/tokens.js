048d54506fc398f40732d9ea2cda3bc7a558fc16
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-04-24 16:49:46 +0300

    create tokens util

---
+++ server/src/utils/tokens.js
@@ -0,0 +1,31 @@
+import { v4 as uuidv4 } from 'uuid';
+
+import { logedUsers } from '../logedUsersDB.js';
+
+export function generateToken() {
+    const token = uuidv4();
+    return token;
+}
+
+export function validateToken(token) {
+
+    let user = logedUsers.find(u => u.token === token);
+    if (!user) {
+        return false
+    }
+    user.timeStamp = new Date().getTime();
+    return true;
+}
+
+export function clearTokens() {
+    setInterval(() => {
+        console.log('Loged users:', logedUsers)
+        let timeStamp = new Date().getTime();
+        for (let i = 0; i < logedUsers.length; i++) {
+            if (timeStamp - logedUsers[i].timeStamp > 300000) {
+                logedUsers.splice(i, 1);
+            }
+        }
+        // console.log(timeStamp)
+    }, 60000);
+}
