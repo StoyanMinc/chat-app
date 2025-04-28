f96fe6558ee1097698e3133b1dc2c985b5469b25
Stoyan Minchev <154098583+StoyanMinc@users.noreply.github.com>
2025-04-28 19:37:21 +0300

    Create requester

---
+++ client/src/api/requester.js
@@ -0,0 +1,43 @@
+const BASE_URL = import.meta.env.VITE_SERVER_BASE_URL;
+import getToken from "../utils/getToken";
+
+async function requester(method, url, data) {
+    // debugger;
+    console.log(BASE_URL);
+    const token = getToken();
+    
+    const options = {
+        method: method,
+        headers: {}
+    }
+
+    if (data) {
+        options.headers['Content-Type'] = 'application/json';
+        options.body = JSON.stringify(data);
+    }
+    console.log(options);
+    if (token) {
+        options.headers['token'] = token;
+    }
+
+    const response = await fetch(`${BASE_URL}${url}`, options);
+
+    if (!response.ok) {
+        const error = await response.json();
+        if (error.code === 403) {
+            localStorage.removeItem('auth');
+        }
+        throw new Error(error.message);
+    }
+
+    const result = await response.json();
+
+    return result;
+}
+
+const get = (url) => requester('GET', url);
+const post = (url, data) => requester('POST', url, data);
+const put = (url, data) => requester('PUT', url, data);
+const del = (url) => requester('DELETE', url);
+
+export { get, post, put, del };
